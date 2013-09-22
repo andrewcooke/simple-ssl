c0485077a0a494a9415764d372bb2f815ee04a95
andrew cooke <andrew@acooke.org>
2013-09-22 16:31:29 -0300

    initial cmd structure

---
+++ src/script/cmd.py
@@ -0,0 +1,88 @@
+
+# the classes here are intended to support the writing of scripts that
+# require input from the user.  the input can be supplied on the command
+# line when invoking the script, or as an interactive prompt.  each input
+# value is associated with an attribute.
+
+# on the class, the attribute defines what inputs are to be read from the
+# command line.  on the instance, attribute access triggers prompting if
+# required.
+
+# conversion from class to instance attribute is done by CmdBase.__new__;
+# the command line functionality is carefully separated into argparse-specific
+# classes (which can be replaced to support optparse, or whatever).
+
+# typical use case:
+# - programmer defines a script using classes, marking attributes
+# - at run time:
+#   - CmdMeta creates class, setting names on attributes.
+#   - ArgPRun collects ArgPOpt attributes, generates appropriate argparse
+#     specification, parses command line and instantiates the class.
+#   - CmdBase converts class to instance attributes on creation.
+#   - Command line values are passed to the __init__, where they are
+#     used to set values.
+#   - the __call__ method triggers script execution.
+#   - when an attribute is accessed, it may prompt the user, cache the
+#     value, etc.
+
+
+class ClsAttr:
+    '''
+    Base class for class attributes.  This is the class that CmdMeta
+    checks for to set the name.
+    '''
+
+    def __init__(self, itype, name=None, *args, **kargs):
+        self._itype = itype
+        self._name = name
+        self._args = args
+        self._kargs = kargs
+
+    def set_name(self, name):
+        if not self._name: self._name = name
+
+    def to_instance(self):
+        return self._itype(name=self._name, *self._args, **self._kargs)
+
+
+class InsAttr:
+    '''
+    Base class for instance attributes.  Subclasses exist on instances and
+    are responsible for prompting the user, caching values, etc.
+    '''
+
+    def __init__(self, name):
+        self._name = name
+
+
+class CmdMeta(type):
+    '''
+    Metaclass that configures class attributes as command line parameters.
+    '''
+
+    def __init__(cls, name, bases, dct):
+        super(CmdMeta, cls).__init__(name, bases, dct)
+        for name, value in dct.items():
+            if isinstance(value, ClsAttr): value.set_name(name)
+
+
+class CmdBase(metaclass=CmdMeta):
+    '''
+    Converts class to instance attributes.  Users should subclass this (or a
+    child).
+    '''
+
+    def __new__(cls, *args, **kargs):
+        instance = super(CmdBase, cls).__new__(cls, *args, **kargs)
+        for name, value in vars(cls).items():
+            if isinstance(value, ClsAttr):
+                setattr(instance, name, value.to_instance())
+        return instance
+
+
+class Cmd(CmdBase):
+    '''
+    Extends CmdBase with useful utilities.
+    '''
+
+    debug = ClsAttr(InsAttr)
---
+++ src/script/test/cmd.py
@@ -0,0 +1,16 @@
+
+from unittest import TestCase
+from script.cmd import InsAttr, ClsAttr, CmdBase
+
+
+class Foo(CmdBase):
+    foo = ClsAttr(InsAttr)
+
+
+class AttrTest(TestCase):
+    
+    def test_foo(self):
+        assert isinstance(Foo.foo, ClsAttr), type(Foo.foo)
+        foo = Foo()
+        assert foo.foo._name == 'foo', foo.foo._name
+        assert isinstance(foo.foo, InsAttr), type(foo.foo)
